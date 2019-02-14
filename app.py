a085f4b99bed1a380b1c10a1aef63f7348f0f5f6
Qaalib Farah <[si8822fb@gmail.com]>
2019-02-14 11:48:34 -0600

    Initial flask setup.

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask import render_template, request, redirect
+import requests
+from peewee import *
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return render_template('index.html', header="Welcome to my Metro Transit Web App")
-
-
+def get_routes():
+    requests.get()
 if __name__ == '__main__':
     app.run()
---
+++ models.py
@@ -0,0 +1,5 @@
+from peewee import *
+
+db = SqliteDatabase('metro_buses.db')
+
+class Route:
