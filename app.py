3881febd3ce8c082ca2d0693578c3b0c85a83f22
Qaalib Farah <[si8822fb@gmail.com]>
2019-02-21 23:28:10 -0600

    Added a database and completed the api calling successfully on a 30 sec interval

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask import render_template, request, redirect
-import requests
-from peewee import *
+from transitHandler import *
+from apscheduler.schedulers.background import BackgroundScheduler
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    get_vehicle_locations()
+    run_clock_reset()
     return render_template('index.html', header="Welcome to my Metro Transit Web App")
-def get_routes():
-    requests.get()
+def run_clock_reset():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(get_vehicle_locations, 'cron', second=30)
+    scheduler.start()
 if __name__ == '__main__':
     app.run()
+
